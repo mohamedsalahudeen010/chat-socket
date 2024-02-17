713d4577455ef9616d433f11214c77fd560b9c44
Mohamed Salahudeen <110691033+mohamedsalahudeen010@users.noreply.github.com>
2024-02-17 23:08:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { Server } from "socket.io";
 
 const io = new Server({
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://guileless-scone-413445.netlify.app/",
   },
 });
 
